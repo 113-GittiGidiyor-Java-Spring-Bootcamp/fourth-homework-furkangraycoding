765baeea80a74b64704f4cc6abee6ad58a0f3d6d
furkangraycoding <58630854+furkangraycoding@users.noreply.github.com>
2021-09-23 02:07:05 +0300

    Created/Modified Dates added for each entities

--- fourth-homework/src/main/java/dev/patika/fourthhomework/model/AbstractBaseEntity.java
+++ fourth-homework/src/main/java/dev/patika/fourthhomework/model/AbstractBaseEntity.java
@@ -1,17 +1,29 @@
 package dev.patika.fourthhomework.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractBaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @CreatedDate
+    private long createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private long modifiedDate;
+
 }
